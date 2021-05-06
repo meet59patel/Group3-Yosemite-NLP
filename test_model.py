fae56621f3aeaa49caf76557dd84a1ab2a171786
Arkaprbha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-05-06 13:53:47 +0530

    ft: test long wrong answer

--- test_model.py
+++ test_model.py
@@ -41,22 +41,22 @@ for line in Lines4:
 
 
 
-# @pytest.mark.parametrize("a,b", small_correct)
-# def test_small_correct(a,b):
-#     assert assess_answer(a,b,5) == 5
+@pytest.mark.parametrize("a,b", small_correct)
+def test_small_correct(a,b):
+    assert assess_answer(a,b,5) == 5
 
-# @pytest.mark.parametrize("a,b", long_correct)
-# def test_large_correct(a,b):
-#     assert assess_answer(a,b,10) == 10
+@pytest.mark.parametrize("a,b", long_correct)
+def test_large_correct(a,b):
+    assert assess_answer(a,b,10) == 10
 
-# @pytest.mark.parametrize("a,b", small_error)
-# def test_small_correct(a,b):
-#     val = assess_answer(a,b,5)
-#     val = 5 -val
-#     assert val >= 1
+@pytest.mark.parametrize("a,b", small_error)
+def test_small_correct(a,b):
+    val = assess_answer(a,b,5)
+    val = 5 -val
+    assert val >= 1
 
-# @pytest.mark.parametrize("a,b", long_error)
-# def test_large_correct(a,b):
-#     val = assess_answer(a,b,10)
-#     val = 10 -val
-#     assert val >= 2
+@pytest.mark.parametrize("a,b", long_error)
+def test_large_correct(a,b):
+    val = assess_answer(a,b,10)
+    val = 10 -val
+    assert val >= 2
